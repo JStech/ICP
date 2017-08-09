0e73e328684f6d496c02743702273d26676e4dfe
John Stechschulte <john.l.stechschulte@gmail.com>
2017-08-09 16:20:02 -0600

    integrated EM into HMRF ICP iterations

--- matlab/mycp.m
+++ matlab/mycp.m
@@ -1,12 +1,5 @@
-function [tf, iter, matched] = mycp(ref, src, params)
-  global beta gamma
-  beta = 4.0;
-  gamma = 2.0;
+function [tf, iter] = mycp(ref, src, params)
   icp;
-
-  dt_thresh = 0.01;
-  dth_thresh = 0.001;
-  scale_thresh = 0.001;
 
   % we'll work with homogenous coords
   n_ref = size(ref, 1);
@@ -27,16 +20,23 @@ function [tf, iter, matched] = mycp(ref, src, params)
 
   all_matches = cell(0);
   matches = 0;
+  z = ones(params.w, params.h);
   for iter=1:params.iter_max
-    [idx, dist] = knnsearch(kdtree, src);
+    [idx, y] = knnsearch(kdtree, src);
+    y = reshape(y, [params.w params.h]);
 
-    init = -1*ones(params.w, params.h);
-    if matches==0
-      init(find(dist < prctile(dist, 90))) = 1;
-    else
-      init(matches(:,1)) = 1;
+    if iter==1
+      z(find(y > prctile(y, 90))) = -1;
+      % extra EM iteration when we first start
+      theta = M_step(z, y);
+      z = E_step(y, z, theta, params);
     end
-    matches = find_matches(ref, src, idx, dist, init, params);
+
+    % update z, find matches
+    theta = M_step(z, y);
+    z = E_step(y, z, theta, params);
+    matches = [find(z>0) idx(find(z>0))];
+
     all_matches{iter} = matches;
 
     % calculate transformation
@@ -55,86 +55,38 @@ function [tf, iter, matched] = mycp(ref, src, params)
       disp(sprintf('Iter %d; scale %f, translation %f, rotation %f', iter, scale, dt, dth));
     end
 
-    if dt < dt_thresh && dth < dth_thresh && abs(scale-1) < scale_thresh
+    if dt < params.dt_thresh && dth < params.dth_thresh && abs(scale-1) < params.scale_thresh
       break;
     end
   end
   assignin('base', 'matches_mycp', all_matches);
-  matched = matches;
 end
 
-function [matches] = find_matches(ref, src, idx, dist, init, params)
-  global beta gamma
-  assert(all(size(init)==[params.w params.h]))
-  assert(all(~isnan(init(:))))
-  y = reshape(dist, [params.w params.h]);
-  z = init;
+function [z] = E_step(y, z, theta, params)
+  mean_field = [z(2:end,:); zeros(1, params.h)] + [zeros(1, params.h); z(1:end-1,:)] + [z(:,2:end) zeros(params.w, 1)] + [zeros(params.w, 1) z(:,1:end-1)];
+  assignin('base', 'db_params', params);
+  assignin('base', 'db_mean_field', mean_field);
+  assignin('base', 'db_theta', theta);
+  assignin('base', 'db_y', y);
+  r_in = params.beta*mean_field - log(theta.in_std) - (y - theta.in_mean).^2/(2*theta.in_std.^2) + params.gamma;
+  r_in(isnan(r_in)) = params.beta*mean_field(isnan(r_in));
+  r_out = -params.beta*mean_field - log(theta.out_std) - (y - theta.out_mean).^2/(2*theta.out_std.^2) - params.gamma;
+  r_out(isnan(r_out)) = params.beta*mean_field(isnan(r_out));
+  z = 2*exp(r_in) ./ (exp(r_out) + exp(r_in)) - 1;
+  assert(all(~isnan(z(:))));
+end
 
-  if sum(z(:)==1) > 2
-    in_mean = mean(reshape(y(z==1), 1, []), 'omitnan');
-    in_std = std(reshape(y(z==1), 1, []), 'omitnan');
-  else
-    % handle bad initialization (all outliers)--shouldn't happen
-    in_mean = min(y(:));
-    in_std = std(y(:), 'omitnan');
-  end
-
-  if sum(z(:)==-1) > 2
-    out_mean = mean(reshape(y(z==-1), 1, []), 'omitnan');
-    out_std = std(reshape(y(z==-1), 1, []), 'omitnan');
-  else
-    % handle bad initialization (all inliers)--shouldn't happen
-    out_mean = max(y(:));
-    out_std = std(y(:), 'omitnan');
-  end
-  assert(~isnan(in_mean));
-  assert(~isnan(in_std));
-  assert(~isnan(out_mean));
-  assert(~isnan(out_std));
-
-  for i=1:3
-    % E-step
-    last_z = z;
-    mean_field = [z(2:end,:); zeros(1, params.h)] + [zeros(1, params.h); z(1:end-1,:)] + [z(:,2:end) zeros(params.w, 1)] + [zeros(params.w, 1) z(:,1:end-1)];
-    assert(all(~isnan(mean_field(:))));
-    r_in = beta*mean_field - log(in_std) - (y - in_mean).^2/(2*in_std.^2) + gamma;
-    r_in(isnan(r_in)) = beta*mean_field(isnan(r_in));
-    r_out = -beta*mean_field - log(out_std) - (y - out_mean).^2/(2*out_std.^2) - gamma;
-    r_out(isnan(r_out)) = beta*mean_field(isnan(r_out));
-    z = 2*exp(r_in) ./ (exp(r_out) + exp(r_in)) - 1;
-    % M-step
-    in_mean = sum((1+z(:))/2.*y(:), 'omitnan')...
-      /sum((1+z(:))/2, 'omitnan');
-    in_std = sqrt(sum((1+z(:))/2.*y(:).^2, 'omitnan')...
-      /sum((1+z(:))/2, 'omitnan') - in_mean.^2);
-    out_mean = sum((1-z(:))/2.*y(:), 'omitnan')...
-      /sum((1-z(:))/2, 'omitnan');
-    out_std = sqrt(sum((1-z(:))/2.*y(:).^2, 'omitnan')...
-      /sum((1-z(:))/2, 'omitnan') - out_mean.^2);
-    if in_mean >= out_mean && params.debug
-      dbob.i = i;
-      dbob.init = init;
-      dbob.w = params.w;
-      dbob.h = params.h;
-      dbob.r_in = r_in;
-      dbob.r_out = r_out;
-      dbob.z = z;
-      dbob.last_z = last_z;
-      dbob.y = y;
-      dbob.in_mean = in_mean;
-      dbob.out_mean = out_mean;
-      dbob.in_std = in_std;
-      dbob.out_std = out_std;
-      assignin('base', 'mycp_debug', dbob);
-    end
-    if (in_mean < out_mean)
-      % this is a bad sign
-      break
-    end
-    assert(~any(isnan([in_mean, in_std, out_mean, out_std])));
-    if ~any(z(:) .* last_z(:) < 0)
-      break
-    end
-  end
-  matches = [find(z > 0) idx(find(z>0))];
+function [theta] = M_step(z, y)
+  theta.in_mean = sum((1+z(:))/2.*y(:), 'omitnan') ...
+    /sum((1+z(:))/2, 'omitnan');
+  theta.in_std = sqrt(sum((1+z(:))/2.*y(:).^2, 'omitnan') ...
+    /sum((1+z(:))/2, 'omitnan') - theta.in_mean.^2);
+  theta.out_mean = sum((1-z(:))/2.*y(:), 'omitnan') ...
+    /sum((1-z(:))/2, 'omitnan');
+  theta.out_std = sqrt(sum((1-z(:))/2.*y(:).^2, 'omitnan') ...
+    /sum((1-z(:))/2, 'omitnan') - theta.out_mean.^2);
+  assert(~isnan(theta.in_mean))
+  assert(~isnan(theta.in_std))
+  assert(~isnan(theta.out_mean))
+  assert(~isnan(theta.out_std))
 end
