fe212d75fd621db73b0e141d647e628ef9537d5d
John Stechschulte <john.l.stechschulte@gmail.com>
2017-12-21 11:21:13 -0500

    also dealing with multiple src_frames

--- matlab/hmrf_icp_test.m
+++ matlab/hmrf_icp_test.m
@@ -40,7 +40,7 @@ ref_frame = 70;
 src_frames = [27 53 247 257 260 267 273 287 334 370 394 418 422 434 455 464 ...
 517 568 628 647 653 662 666 677 686 689 693 703 706 719 726 747 803 844 878 ...
 879 889 897 903 936] + ref_frame;
-src_frames = 763;
+%src_frames = 763;
 %ref_frame = 156;
 %src_frames = [15 35 38 39 41 47 89 129 133 135 148 150 151 154 155 157 158 ...
 %159 160 162 163 167 171 181 184 190 197 204 205 219 256 271 282 290 301 313 ...
@@ -66,7 +66,8 @@ for angle_i=1:length(angles)
     t_init = eye(4);
     t_init(1:3,1:3) = aa2mat(axis, angle);
     params.t_init = t_init;
-    for src_frame=src_frames
+    for src_frame_i=1:length(src_frames)
+      src_frame = src_frames(src_frame_i);
       c2 = downsample(unproject(getcloud(src_frame)), 640, 480, 2);
       true_tf = inv(pmats{ref_frame})*pmats{src_frame};
       c2_t = (true_tf*c2')' - origin;
@@ -82,17 +83,17 @@ for angle_i=1:length(angles)
         [tf iters] = icp(c1, c2_t, params);
         elapsed = toc;
         tf_log = se3log(tf);
-        results.(m){angle_i, axis_i} = [iters, elapsed, norm(tf_log(1:3)), norm(tf_log(4:6))];
+        results.(m){angle_i, axis_i, src_frame_i} = [iters, elapsed, norm(tf_log(1:3)), norm(tf_log(4:6))];
       end
       try
         tic;
         [tf data] = hmrf_icp(c1, c2_t, params);
         elapsed = toc;
         tf_log = se3log(tf);
-        results.hmrf{angle_i, axis_i} = [data.icp_iters, elapsed, ...
-          norm(tf_log(1:3)), norm(tf_log(4:6)), data.em_iters];
+        results.hmrf{angle_i, axis_i, src_frame_i} = [data.icp_iters, ...
+          elapsed, norm(tf_log(1:3)), norm(tf_log(4:6)), data.em_iters];
       catch ae
-        results.hmrf{angle_i, axis_i} = nan;
+        results.hmrf{angle_i, axis_i, src_frame_i} = nan;
       end
     end
   end
