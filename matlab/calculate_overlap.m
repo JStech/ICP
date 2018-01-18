a1783b58c674b175e1c3c8770228a93d97cf0bb2
John Stechschulte <john.l.stechschulte@gmail.com>
2018-01-18 15:47:42 -0700

    more stuff for better tests

--- matlab/calculate_overlap.m
+++ matlab/calculate_overlap.m
@@ -4,5 +4,5 @@ function [overlap] = calculate_overlap(c1, c2)
   [idx1, dist1] = knnsearch(kdtree1, c2);
   [idx2, dist2] = knnsearch(kdtree2, c2, 'K', 2);
 
-  overlap = sum(dist1 < 8*dist2(:,2), 'omitnan')/sum(~isnan(dist2(:,2)));
+  overlap = sum(dist1 < 6*dist2(:,2), 'omitnan')/sum(~isnan(dist2(:,2)));
 end
---
+++ matlab/sample_frames.m
@@ -0,0 +1,20 @@
+n = size(overlaps, 1);
+step = 6;
+
+ols = [0 0 0];
+for i=1:step:n
+  for j=1:step:n
+    ols = [ols; overlaps(i, j) i j];
+  end
+end
+
+[~, sorted_order] = sort(ols(:,1));
+
+selected = [];
+pairs_per_decile = 10;
+for decile=0.1:0.1:1
+  selected = [selected
+  datasample(find(decile-0.1 < ols(:,1) & ols(:,1) < decile), pairs_per_decile)];
+end
+
+selected = ols(selected, :);
