aaf94a08547475c61b2934a89d3b43deb2aad8b0
John Stechschulte <john.l.stechschulte@gmail.com>
2018-05-08 11:49:53 -0600

    consolidated load_data.m

--- matlab/load_desk_data.m
+++
@@ -1,8 +0,0 @@
-if ~exist('cloudLoc') || size(cloudLoc, 1) ~= 595
-  load('../data/freiburg1_desk.mat');
-  assignin('base', 'cloudLoc', cloudLoc);
-end
-
-project = @(pts) pts(:,1:3)./pts(:,4);
-unproject = @(pts) [pts ones(size(pts, 1), 1)];
-getcloud = @(i) squeeze(cloudLoc(i,:,:));
--- matlab/load_room_data.m
+++
@@ -1,8 +0,0 @@
-if ~exist('cloudLoc') || size(cloudLoc, 1) ~= 1360
-  load('../data/freiburg1_room.mat');
-  assignin('base', 'cloudLoc', cloudLoc);
-end
-
-project = @(pts) pts(:,1:3)./pts(:,4);
-unproject = @(pts) [pts ones(size(pts, 1), 1)];
-getcloud = @(i) squeeze(cloudLoc(i,:,:));
