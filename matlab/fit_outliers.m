ce5fbb72550aa587717f807cc657aac516382582
John Stechschulte <john.l.stechschulte@gmail.com>
2018-05-14 13:55:57 -0600

    outlier fitting across datasets

--- matlab/fit_outliers.m
+++ matlab/fit_outliers.m
@@ -1,45 +1,45 @@
-num_samples = 100;
-k = 150;
+for dset={'shark' 'desk' 'room'}
+  dataset = dset{1};
+  load_data; load_poses;
+  num_samples = 100;
+  k = 150;
 
-% TODO: is this list right?
-distributions = {
-  'Burr',             %  1
-  'Exponential',      %  2
-  'Gamma',            %  3
-  'HalfNormal',       %  4
-  'Logistic',         %  5
-  'Loglogistic',      %  6
-  'Lognormal',        %  7
-  'Normal',           %  8
-  'Poisson',          %  9
-  'Rayleigh',         % 10
-  'Rician',           % 11
-  'tLocationScale',   % 12
-  };
+  % TODO: is this list right?
+  distributions = {
+    'Gamma',            % 1
+    'Logistic',         % 2
+    'Loglogistic',      % 3
+    'Lognormal',        % 4
+    'Normal',           % 5
+    'Rayleigh',         % 6
+    'tLocationScale',   % 7
+    };
 
-results = cell(0);
+  results = cell(0);
 
-for i=1:num_samples
-  i
-  f1 = randi(n-k, 1);
-  f2 = f1 + randi(k, 1);
-  t1 = pmats{f1};
-  t2 = pmats{f2};
-  c1 = unproject(getcloud(f1));
-  c2 = unproject(getcloud(f2));
-  results(i, :) = {f1, f2, zeros(1, length(distributions))};
+  for i=1:num_samples
+    disp(sprintf('%s %d', dataset, i));
+    f1 = randi(n-k, 1);
+    f2 = f1 + randi(k, 1);
+    t1 = pmats{f1};
+    t2 = pmats{f2};
+    c1 = unproject(getcloud(f1));
+    c2 = unproject(getcloud(f2));
+    results(i, :) = {f1, f2, zeros(1, length(distributions))};
 
-  [~, ~, ~, y_out_al] = y_dists(c1, c2, inv(inv(t1)*t2));
-  [f, x] = ecdf(y_out_al);
+    [~, ~, ~, y_out_al] = y_dists(c1, c2, inv(inv(t1)*t2));
+    [f, x] = ecdf(y_out_al);
 
-  for d=1:length(distributions)
-    try
-      pd = fitdist(y_out_al, distributions{d});
-      ks = max(abs(f - pd.cdf(x)));
-      results{i, 3}(d) = ks;
-    catch
-      disp(sprintf('Skipping %s due to error', distributions{d}))
+    for d=1:length(distributions)
+      try
+        pd = fitdist(y_out_al, distributions{d});
+        ks = max(abs(f - pd.cdf(x)));
+        results{i, 3}(d) = ks;
+      catch
+        disp(sprintf('Skipping %s due to error', distributions{d}))
+      end
+      %pause(3)
     end
-    pause(1)
   end
+  all_results.(dataset) = results;
 end
